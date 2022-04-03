773a6e6eddf5a9e7503c8fdc56fff4129b953d4d
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-03 12:08:25 +0530

    🔧 chore : update storybook config file to provide AppProvider to all stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import RemodalProvider from '../src/styles/Provider/Provider';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions  : { argTypesRegex: '^on[A-Z].*' },
+	controls : {
+		matchers : {
+			color : /(background|color)$/i,
+			date  : /Date$/
+		}
+	}
+};
+
+export const decorators = [
+	(Story) => (
+		<RemodalProvider theme="light">
+			<Story />
+		</RemodalProvider>
+	)
+];
