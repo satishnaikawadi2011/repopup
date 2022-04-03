e16d78b49c7ea7f1b2ebb2dc75a273743e649862
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-03 11:19:25 +0530

    🔧 refactor : create styled.d.ts file to extend the existing styled-component DefaultTheme

---
+++ src/styled.d.ts
@@ -0,0 +1,23 @@
+// import original module declarations
+import 'styled-components';
+import { IPalette } from './themes/types';
+
+// and extend them!
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		palette: {
+			common: {
+				black: string;
+				white: string;
+				bgColor: string;
+				textColor: string;
+				backdropColor: string;
+			};
+			primary: IPalette;
+			secondary: IPalette;
+			success: IPalette;
+			error: IPalette;
+			cancel: IPalette;
+		};
+	}
+}
