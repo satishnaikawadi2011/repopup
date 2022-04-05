8aa7582cf18062cd8790f0996b6550fa386de394
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-05 11:57:37 +0530

    ✨ feat : add a placement prop to Actions component

--- src/components/Button/Actions/Actions.tsx
+++ src/components/Button/Actions/Actions.tsx
@@ -1,12 +1,34 @@
 import styled from 'styled-components';
 
-export const Actions = styled.div`
+interface IActionsProps {
+	/**
+	 * Use this for the placement of the buttons inside the container 
+	 */
+	placement?:
+		| 'space-around'
+		| 'space-between'
+		| 'space-evenly'
+		| 'stretch'
+		| 'center'
+		| 'end'
+		| 'flex-end'
+		| 'flex-start'
+		| 'start';
+}
+
+export const Actions =
+	styled.div <
+	IActionsProps >
+	`
 	display: flex;
 	z-index: 1;
 	box-sizing: border-box;
 	flex-wrap: wrap;
 	align-items: center;
-	justify-content: center;
+	justify-content: ${(props) =>
+
+			props.placement ? props.placement :
+			'center'};
 	width: auto;
 	margin: 1.25em auto 0;
 	padding: 0;
