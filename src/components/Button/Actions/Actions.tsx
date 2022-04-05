98f0e48ef9fc9efff88feb9ac7f5df64c68facff
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-05 11:41:58 +0530

    ✨ feat : add a Actions component which will be a wrapper of Buttons in Modal Component

---
+++ src/components/Button/Actions/Actions.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Actions = styled.div`
+	display: flex;
+	z-index: 1;
+	box-sizing: border-box;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: center;
+	width: auto;
+	margin: 1.25em auto 0;
+	padding: 0;
+`;
