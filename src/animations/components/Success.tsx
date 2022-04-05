ad0ab473b0889f1b341b2f6273526ca4fe45a80d
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-05 13:52:34 +0530

    ✨ feat : add Error,Info,Warning,Question and Success Icon components

---
+++ src/animations/components/Success.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Lottie, { Options } from 'react-lottie';
+
+import animationData from '../data/success.json';
+
+const defaultOptions: Options = {
+	loop: true,
+	autoplay: true,
+	animationData: animationData,
+	rendererSettings:
+		{
+			preserveAspectRatio: 'xMidYMid slice'
+		}
+};
+
+interface Props {
+	height?: number;
+	width?: number;
+}
+
+const SuccessIcon: React.FC<Props> = ({ height = 100, width = 100 }) => {
+	return <Lottie options={defaultOptions} height={height} width={width} />;
+};
+
+export default SuccessIcon;
