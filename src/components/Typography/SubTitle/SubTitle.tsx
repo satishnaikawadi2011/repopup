f5d26bba0476707c681813ff2502c5400bc708d3
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-05 12:10:54 +0530

    ✨ feat : add a SubTitle component

---
+++ src/components/Typography/SubTitle/SubTitle.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const SubTitle = styled.div`
+	z-index: 1;
+	justify-content: center;
+	margin: 1em 1.6em 0.3em;
+	padding: 0;
+	overflow: auto;
+	color: inherit;
+	font-size: 1.125em;
+	font-weight: 400;
+	line-height: normal;
+	text-align: center;
+	word-wrap: break-word;
+	word-break: break-word;
+`;
+
+export default SubTitle;
