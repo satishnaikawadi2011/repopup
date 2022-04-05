0e11ff333e867150797126fbfca83beeeaa995be
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-05 12:07:08 +0530

    ✨ feat : add a Title component

---
+++ src/components/Typography/Title/Title.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+	position: relative;
+	max-width: 100%;
+	margin: 0;
+	padding: .8em 1em 0;
+	color: inherit;
+	font-size: 1.875em;
+	font-weight: 600;
+	text-align: center;
+	text-transform: none;
+	word-wrap: break-word;
+`;
+
+export default Title;
