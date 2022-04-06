4b9ffa882f192288c1f8b7a8e789bc1452518d6f
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-06 13:35:33 +0530

    📚 chore : add stories for a Alert Component

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ConfirmButton } from '../Button/Button';
+import Alert from './Alert';
+
+export default {
+	/* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+	title: 'Alert',
+	component: React.Fragment
+} as ComponentMeta<typeof React.Fragment>;
+
+export const Success: ComponentStory<typeof React.Fragment> = () => {
+	const [
+		open,
+		setOpen
+	] = React.useState(false);
+	return (
+		<React.Fragment>
+			<ConfirmButton onClick={() => setOpen(true)}>Open Alert</ConfirmButton>
+			<Alert
+				open={open}
+				onClose={() => setOpen(false)}
+				title={'Success'}
+				subtitle={`Task completed successfully.`}
+				variant="success"
+			/>
+		</React.Fragment>
+	);
+};
+
+export const Error: ComponentStory<typeof React.Fragment> = () => {
+	const [
+		open,
+		setOpen
+	] = React.useState(false);
+	return (
+		<React.Fragment>
+			<ConfirmButton onClick={() => setOpen(true)}>Open Alert</ConfirmButton>
+			<Alert
+				open={open}
+				onClose={() => setOpen(false)}
+				title={'Oops'}
+				subtitle={`Something went wrong,try again!`}
+				variant="error"
+			/>
+		</React.Fragment>
+	);
+};
+
+export const Info: ComponentStory<typeof React.Fragment> = () => {
+	const [
+		open,
+		setOpen
+	] = React.useState(false);
+	return (
+		<React.Fragment>
+			<ConfirmButton onClick={() => setOpen(true)}>Open Alert</ConfirmButton>
+			<Alert
+				open={open}
+				onClose={() => setOpen(false)}
+				title={'Note'}
+				variant="info"
+				subtitle="Please follow the instructions below."
+			/>
+		</React.Fragment>
+	);
+};
+
+export const Warning: ComponentStory<typeof React.Fragment> = () => {
+	const [
+		open,
+		setOpen
+	] = React.useState(false);
+	return (
+		<React.Fragment>
+			<ConfirmButton onClick={() => setOpen(true)}>Open Alert</ConfirmButton>
+			<Alert
+				open={open}
+				onClose={() => setOpen(false)}
+				title={'Warning'}
+				variant="warning"
+				subtitle="10 minutes before full time."
+			/>
+		</React.Fragment>
+	);
+};
+
+export const Question: ComponentStory<typeof React.Fragment> = () => {
+	const [
+		open,
+		setOpen
+	] = React.useState(false);
+	return (
+		<React.Fragment>
+			<ConfirmButton onClick={() => setOpen(true)}>Open Alert</ConfirmButton>
+			<Alert
+				open={open}
+				onClose={() => setOpen(false)}
+				title={'Answer It'}
+				variant="question"
+				subtitle={`Is it ok to play games?`}
+			/>
+		</React.Fragment>
+	);
+};
