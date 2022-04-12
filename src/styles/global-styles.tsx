ee33e7e8c81f689146c75a3a56563d6c1495c5e2
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-12 21:30:10 +0530

    🐛 fix : update globalStyles of styled-components to use @font-faces successfully.

--- src/styles/global-styles.tsx
+++ src/styles/global-styles.tsx
@@ -1,6 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
+import QuicksandRegular from '../fonts/Quicksand-Regular.ttf';
+import QuicksandBold from '../fonts/Quicksand-Bold.ttf';
+import QuicksandSemiBold from '../fonts/Quicksand-SemiBold.ttf';
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+	font-family: "QuicksandRegular";
+	src: local("QuicksandRegular"), url(${QuicksandRegular}) format("truetype");
+	font-weight: normal;
+}
+
+@font-face {
+	font-family: "QuicksandBold";
+	src: local("QuicksandBold"), url(${QuicksandBold}) format("truetype");
+	font-weight: bold;
+}
+@font-face {
+	font-family: "QuicksandSemiBold";
+	src: local("QuicksandSemiBold"), url(${QuicksandSemiBold}) format("truetype");
+	font-weight: bold;
+}
 *,
 body {
 	font-family: QuicksandRegular;
