c5f471135fed39ec88817ef1a3a79b747241fa80
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-15 02:55:52 +0530

    🔖 chore : update version in package.json before new release

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,6 @@
+import useInput from './useInput';
 import useAlert from './useAlert';
-export { useAlert };
+import useTextArea from './useTextArea';
+import useSelect from './useSelect';
+import useModal from './useModal';
+export { useAlert, useInput, useTextArea, useSelect, useModal };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import { Alert } from './components/Alert';
 import RepopupProvider from './styles/Provider/Provider';
 import { Select as SelectDialog, Input as InputDialog, TextArea as TextAreaDialog } from './components/Input';
 import { Modal } from './components/Modal';
-import { useAlert } from './hooks';
+import { useAlert, useInput, useTextArea, useSelect, useModal } from './hooks';
 
-export { Alert, SelectDialog, InputDialog, TextAreaDialog, Modal, RepopupProvider };
+export {
+	Alert,
+	SelectDialog,
+	InputDialog,
+	TextAreaDialog,
+	Modal,
+	RepopupProvider,
+	useAlert,
+	useInput,
+	useTextArea,
+	useSelect,
+	useModal
+};
