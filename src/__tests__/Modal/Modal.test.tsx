e74ced2854c432dfdbe76caeca82df8c3fff25cc
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-15 02:47:39 +0530

    ✅ chore : add some tests for Modal component

--- src/__tests__/Modal/Modal.test.tsx
+++ src/__tests__/Modal/Modal.test.tsx
@@ -8,10 +8,24 @@ describe('<Modal/>', () => {
 		const onRequestCloseMock = jest.fn();
 		render(
 			<RepopupProvider>
-				<Modal open={true} onRequestClose={onRequestCloseMock} />
+				<Modal open={false} onRequestClose={onRequestCloseMock} />
 			</RepopupProvider>
 		);
 		expect(screen.getByTestId('modal-container')).toBeInTheDocument();
 		expect(screen.getByTestId('popup-window')).toBeInTheDocument();
+	});
+});
+
+describe('when rendered with children', () => {
+	it('should render that children', () => {
+		const onRequestCloseMock = jest.fn();
+		render(
+			<RepopupProvider>
+				<Modal open={false} onRequestClose={onRequestCloseMock}>
+					<h1>This is Test</h1>
+				</Modal>
+			</RepopupProvider>
+		);
+		expect(screen.getByText(/This is Test/)).toBeInTheDocument();
 	});
 });
