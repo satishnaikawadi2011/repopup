0071b8f4d700c7490615f89156174c441bab9256
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2022-04-15 01:10:18 +0530

    🔧 chore : update jest config to get it up and running

--- jest.config.js
+++ jest.config.js
@@ -24,5 +24,6 @@ module.exports = {
 		'\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$' :
 			'identity-obj-proxy',
 		'\\.(css|less|scss|sass)$'                                                                : 'identity-obj-proxy'
-	}
+	},
+	testEnvironment        : 'jsdom'
 };
